3f1ee68cf94684693e77dc39a21e6037332bc9c0
Brittany V <brittbyu@gmail.com>
2018-06-14 18:57:00 -0600

    initial commit

---
+++ app/controllers/api/songs_controller.rb
@@ -0,0 +1,29 @@
+class Api::SongsController < ApplicationController
+  def index
+    render json Song.all
+  end
+
+  def create
+    song = Song.new(song_params)
+    if song.save
+      render json: song
+    else
+      render json: { errors: song.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    song = Song.find(params[:id])
+    song.update(comlete: !song.complete)
+    render json: song
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def song_params
+  end
+
+end
